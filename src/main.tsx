f0b7444447c182a8e53e5e5b71cd5e5002af1415
Artem Frolov <artfrol.af@gmail.com>
2025-05-06 00:41:49 +0300

    host on gh-pages and set veyalab.ru domain

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,15 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.tsx';
+import './index.css';
 
-createRoot(document.getElementById("root")!).render(<App />);
+const baseName =
+  import.meta.env.MODE === 'development' || window.location.hostname === 'veyalab.ru'
+    ? '/'
+    : '/veyalab-website-ru';
+
+createRoot(document.getElementById('root')!).render(
+  <BrowserRouter basename={baseName}>
+    <App />
+  </BrowserRouter>
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,28 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import fs from 'fs';
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+export default defineConfig(({ mode }) => {
+  // кастомный хост из env или дефолтный
+  const isCustomDomain = process.env.CUSTOM_DOMAIN === 'true';
+
+  return {
+    base: "./",
+    
+    plugins: [
+      react(),
+      {
+        name: 'copy-index-to-404',
+        closeBundle: () => {
+          fs.copyFileSync('dist/index.html', 'dist/404.html');
+        }
+      }
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      }
+    }
+  };
+});
